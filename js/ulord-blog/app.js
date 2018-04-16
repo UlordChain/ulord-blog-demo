0f677f22814d77368fe9fb9e392ed21f31b39260
xiaoshuji <719355782@qq.com>
2018-04-16 17:26:37 +0800

    ulord-blog-demo  js code

---
+++ js/ulord-blog/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var ejs = require('ejs');
+var session = require('express-session')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.engine('html',ejs.__express);
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({
+  secret: '12345',
+  name: 'testapp',
+  cookie: {maxAge: 60000},
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ js/ulord-blog/routes/users.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+
+module.exports = router;
